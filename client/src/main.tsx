aef2469af484c06850c0724d9b20d9e42f7fde4d
Namgel <namgeltamang14@gmail.com>
2023-08-27 19:32:11 +0545

    Build product page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,11 +12,13 @@ import {
 } from 'react-router-dom';
 import './index.css';
 import HomePage from './pages/HomePage.tsx';
+import ProductPage from './pages/ProductPage.tsx';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<App />}>
 			<Route index={true} path="/" element={<HomePage />} />
+			<Route path="/product/:id" element={<ProductPage />} />
 		</Route>
 	)
 );
