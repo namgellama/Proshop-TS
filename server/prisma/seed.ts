462145a6eac8dba092d4ea451588a44f31fcc7bb
Namgel <namgeltamang14@gmail.com>
2023-08-28 15:51:47 +0545

    Create asyncHandler and errorHandler middleware

---
+++ server/prisma/seed.ts
@@ -0,0 +1,89 @@
+import { db } from '../src/config/db.server';
+import products from '../src/data/products';
+
+interface Proudct {
+	name: string;
+	image: string;
+	description: string;
+	brand: string;
+	category: string;
+	price: number;
+	countInStock: number;
+	rating: number;
+	numReviews: number;
+}
+
+interface User {
+	name: string;
+	email: string;
+	password: string;
+	isAdmin: boolean;
+}
+
+async function seed() {
+	// await Promise.all(
+	// 	getUsers().map((user) => {
+	// 		return db.user.create({
+	// 			data: {
+	// 				name: user.name,
+	// 				email: user.email,
+	// 				password: user.password,
+	// 				isAdmin: user.isAdmin,
+	// 			},
+	// 		});
+	// 	})
+	// );
+
+	const user = await db.user.findFirst({
+		where: {
+			id: 1,
+		},
+	});
+
+	await Promise.all(
+		getProducts().map((product) => {
+			const {
+				name,
+				image,
+				description,
+				brand,
+				category,
+				price,
+				countInStock,
+				rating,
+				numReviews,
+			} = product;
+			return db.product.create({
+				data: {
+					name,
+					image,
+					description,
+					brand,
+					category,
+					price,
+					countInStock,
+					rating,
+					numReviews,
+					userId: user?.id || 0,
+				},
+			});
+		})
+	);
+}
+
+seed();
+
+function getUsers(): Array<User> {
+	return [
+		{
+			name: 'admin',
+			email: 'admin@gmail.com',
+			password: 'admin',
+			isAdmin: true,
+		},
+	];
+}
+
+function getProducts(): Array<Proudct> {
+	return products;
+}
